16b60a252d2aeae2d9b7f9726749b34a1b245f8d
Rabies Cow <rabiescow@proton.me>
2025-03-22 13:08:33 -0400

    updated ll to use eza instead of exa

--- functions/lla.fish
+++ functions/lla.fish
@@ -3,5 +3,5 @@
 #
 function lla --wraps ls --description "List contents of directory using long format"
     # ls -lh $argv
-  exa -halgm@r --git --icons --time-style full-iso --reverse --sort modified $argv
+  eza -halgm@r --git --icons --time-style full-iso --reverse --sort modified $argv
 end
--- functions/llx.fish
+++ functions/llx.fish
@@ -2,5 +2,5 @@
 #
 function llx --wraps ls --description "List contents of directory using long format"
     # ls -lh $argv
-  exa -halgm@T --git --icons --time-style full-iso --sort type $argv
+  eza -halgm@T --git --icons --time-style full-iso --sort type $argv
 end
